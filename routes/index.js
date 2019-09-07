adbdd8a6a20f12cef071db96b6495095a274a2fd
RKOrtega <rkortega1994@gmail.com>
2019-09-07 12:24:05 -0500

    Adding template

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//Firebase SDK
+var firebase = require("firebase");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET error page. */
 router.get('/error', function(req, res, next) {
   res.render('error', { title: 'Express' });
 });
