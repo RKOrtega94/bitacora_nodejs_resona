5190b3f47556f8244293070e8c7a0d4d8b3d17d9
RKOrtega <rkortega1994@gmail.com>
2019-09-22 21:52:29 -0500

    Starter project

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
 const session = require('express-session')
 const cors = require('cors')
 const errorHandler = require('errorhandler')
 var createError = require('http-errors')
+var User = require('./model/user')
+var morgan = require('morgan')
 
 //FireBase SDK
 var admin = require("firebase-admin")
@@ -20,6 +23,15 @@ admin.initializeApp({
 // invoke an instance of express application.
 var app = express();
 
+// set morgan to log info about our requests for development use.
+app.use(morgan('dev'));
+
+// initialize body-parser to parse incoming parameters requests to req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize cookie-parser to allow us access the cookies stored in the browser. 
+app.use(cookieParser());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -28,35 +40,175 @@ app.set('view engine', 'jade');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+// initialize express-session to allow us track the logged-in user across sessions.
 app.use(session({
-  secret: 'keyboard cat',
+  key: 'user_sid',
+  secret: 'somerandonstuffs',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000
+  }
 }))
 
-app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/logout', logoutRouter);
-app.use('/admin/users', usersRouter);
-app.use('/404', notFoundRouter)
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie('user_sid')
+  }
+  next()
+})
 
-// catch 404 and forward to error handler
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+}
+
+function createTicket(req) {
+  var date = new Date;
+  var day = '' + date.getDate();
+  var month = '' + date.getMonth();
+  var hour = '' + date.getHours();
+  var minutes = '' + date.getMinutes();
+  var seconds = '' + date.getSeconds();
+
+
+  if (day.length <= 1) {
+    day = '0' + day;
+  }
+
+  if (month.length <= 1) {
+    month = '0' + month;
+  }
+
+  if (hour.length <= 1) {
+    hour = '0' + hour;
+  }
+
+  if (minutes.length <= 1) {
+    minutes = '0' + minutes;
+  }
+
+  if (seconds.length <= 1) {
+    seconds = '0' + seconds;
+  }
+
+  var currentDate = day + '/' + month + '/' + date.getFullYear() + ' ' + hour + ':' + minutes + ':' + seconds;
+  var ref = admin.database()
+    .ref('tickets')
+    .child(req.session.user.username)
+    .child(req.body.txtTicket)
+    .set({
+      anillamador: req.body.txtAnillamador,
+      dni: req.body.txtDni,
+      pir: req.body.txtPir,
+      ticket: req.body.txtTicket,
+      tmo: req.body.txtTmo,
+      date: currentDate
+    })
+}
+
+// route for user signup
+app.route('/signup')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/signup.html');
+    })
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+            .then(user => {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            })
+            .catch(error => {
+                res.redirect('/signup');
+            });
+    });
+
+
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.render('login', {
+            title: 'Login'
+        });
+    })
+    .post((req, res) => {
+        var username = req.body.txtUsername,
+            password = req.body.txtPassword;
+
+        User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.render('login', {
+                    title: 'Login',
+                    message: 'Username or password wrong!'
+                });
+            } else if (!user.validPassword(password)) {
+                res.render('login', {
+                    title: 'Login',
+                    message: 'Username or password wrong!'
+                });
+            } else {
+                user.validPassword(password)
+                req.session.user = user.dataValues;
+                res.redirect('/');
+            }
+        })
+    })
+
+
+// route for user's dashboard
+app.get('/', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        var ref = firebase.database().ref('tickets').child(req.session.user.username)
+        console.log("Ref: " + ref)
+        ref.orderByChild('ticket').once('value', snapshot => {
+            snapshot.forEach(function (childSnapshot) {
+                var result = childSnapshot.val()
+                var resultRef = ref.child(result.ticket)
+                resultRef.orderByChild('ticket').once('value', snapshot => {
+                    res.render('index', {
+                        title: 'Home',
+                        name: req.session.user.username,
+                        result: JSON.stringify(snapshot.val())
+                    });
+                })
+            });
+        })
+    } else {
+        res.redirect('/login');
+    }
+})
+
+// route for user's post dashboard
+app.post('/', (req, res) => {
+    createTicket(req)
+    res.redirect('/')
+})
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+})
+
+
+// route for handling 404 requests(unavailable routes)
 app.use(function (req, res, next) {
-  return res.status(404).redirect('/404');
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+    res.status(404).redirect('/404')
+})
 module.exports = app;
---
+++ model/user.js
@@ -0,0 +1,45 @@
+// Imports
+var Sequelize = require('sequelize');
+var bcrypt = require('bcrypt');
+
+// create a sequelize instance with our local postgres database information.
+var sequelize = new Sequelize('postgres://lvozwwbiihhhqp:eba0866a9c3682a970309066ec1fb9ef10b0138492efc73a4e56841d279e3239@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d26go21gq41h86?ssl=true')
+
+// setup User model and its fields.
+var User = sequelize.define(
+    'User', {
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    hooks: {
+        beforeCreate: (user) => {
+            const salt = bcrypt.genSaltSync();
+            user.password = bcrypt.hashSync(user.password, salt);
+        }
+    }
+});
+
+User.prototype.validPassword = function (password) {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(this.password, salt);
+    return bcrypt.compareSync(password, hash)
+}
+
+sequelize.sync()
+    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
+    .catch(error => console.log('This error occured', error));
+
+// export User model for use in other files.
+module.exports = User;
